5e03faa0823bcbe340b58213e6ece79553a015ce
Matt Malarkey <mm5917@imperial.ac.uk>
2018-08-19 17:23:50 +0100

    Moved snake code to run function, setup from command line args

--- snake/snake.py
+++ snake/snake.py
@@ -25,70 +25,76 @@ def legal_move(snake):
         return False
     return True
 
-xLEDs = 34
-yLEDs = 32
+def run(snakeIO):
+    xLEDs = 34
+    yLEDs = 32
 
-snakeIO = DebugIO()
+    score = 0
+    snake = [[4,10], [4,9], [4,8]]
+    food = [10,20]
 
-score = 0
-snake = [[4,10], [4,9], [4,8]]
-food = [10,20]
+    KEY_ESC = 27
+    alive = True
+    key = 0
+    direction = snakeIO.get_start_dir()
 
-KEY_ESC = 27
-alive = True
-key = 0
-direction = snakeIO.get_start_dir()
+    # Display start screen
+    #curses.napms(1000)
 
-# Display start screen
-#curses.napms(1000)
+    while alive:
+        # Increase speed when snake is longer?
+        #snakeIO.wait_for_input(150 - (len(snake)/5 + len(snake)/10)%120)
+        snakeIO.wait_for_input(150)
 
-while alive:
-    # Increase speed when snake is longer?
-    #snakeIO.wait_for_input(150 - (len(snake)/5 + len(snake)/10)%120)
-    snakeIO.wait_for_input(150)
+        # Get key
+        # get_keypress returns -1 if no key is pressed
+        prevKey = key
+        event = snakeIO.get_keypress()
+        key = key if event == -1 else event
 
-    # Get key
-    # get_keypress returns -1 if no key is pressed
-    prevKey = key
-    event = snakeIO.get_keypress()
-    key = key if event == -1 else event
+        # TODO: change control flow of get key press
+        # Change direction if new, valid key pressed
+        if key == KEY_ESC:
+            break
+        if key != prevKey:
+            if key in snakeIO.keys_to_dirs: # TODO change to if then get, default one line
+                direction = change_dir(direction, snakeIO.keys_to_dirs.get(key))
 
-    # TODO: change control flow of get key press
-    # Change direction if new, valid key pressed
-    if key == KEY_ESC:
-        break
-    if key != prevKey:
-        if key in snakeIO.keys_to_dirs: # TODO change to if then get, default one line
-            direction = change_dir(direction, snakeIO.keys_to_dirs.get(key))
+        # Add on the new head to the snake, check if legal
+        snake.insert(0, list(map(add, snake[0], direction)))
+        if not legal_move(snake):
+            break
 
-    # Add on the new head to the snake, check if legal
-    snake.insert(0, list(map(add, snake[0], direction)))
-    if not legal_move(snake):
-        break
-
-    # If food is eaten, generate new, otherwise remove tail
-    if snake[0] == food:
-        score += 1
-        last = []
-        food = [randint(1, yLEDs-2), randint(1, xLEDs-2)]
-        while food in snake:
+        # If food is eaten, generate new, otherwise remove tail
+        if snake[0] == food:
+            score += 1
+            last = []
             food = [randint(1, yLEDs-2), randint(1, xLEDs-2)]
-    else:
-        trail = snake.pop()
+            while food in snake:
+                food = [randint(1, yLEDs-2), randint(1, xLEDs-2)]
+        else:
+            trail = snake.pop()
 
-    # Output new frame and score, respective IO class will update display as necessary
-    snakeIO.output(snake, trail, food)
-    snakeIO.debug_score(score)
+        # Output new frame and score, respective IO class will update display as necessary
+        snakeIO.output(snake, trail, food)
+        snakeIO.debug_score(score)
 
 
-# Display game over screen
+    # Display game over screen
 
 
-snakeIO.cleanup()
-print("\nScore - " + str(score))
+    snakeIO.cleanup()
+    print("\nScore - " + str(score))
 
-#if __name__ == '__main__':
+if __name__ == '__main__':
 	# Set mode based on command line args
 	# 0 = game mode
-	# 1 = dev mode (don't use joysticks, send data to opengl server)
-	#assert (len(sys.argv) == 2), 'Incorrect number of arguments supplied'
+	# 1 = dev mode (don't use joysticks, terminal simulation)
+    # 2 = dev mode as above but send output to simulation also
+    # 3 = dev mode as above but send output to LEDs also
+	assert (len(sys.argv) == 2), 'Incorrect number of arguments supplied'
+    if sys.args[1] == 0:
+        snakeIO = LEDIO()
+    else:
+        snakeIO = DebugIO()
+    run(snakeIO)
